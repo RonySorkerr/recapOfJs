e2b9586ecb41ac9f4386ea4f047302fa9eaa5c9f
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-21 20:51:17 -0800

    first commit

---
+++ concate.js
@@ -0,0 +1,7 @@
+const firstName = 'rony';
+const lastName = 'sarakr';
+const age = 22;
+const gender = 'male';
+
+const fullInfo = `Mr . ${firstName} ${lastName} your age is ${age} and your gender is ${gender}`;
+console.log(fullInfo);
---
+++ loop.js
@@ -0,0 +1,6 @@
+let name = 'I am rony sorker';
+let count = 0;
+for(let i = 10; i < 20; i--){
+    console.log(i);
+}
+
